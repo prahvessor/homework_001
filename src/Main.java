419187871f369551b589261ccbf526e87e9d9969
Emin Aliiev <emin.aliev@gmail.com>
2015-11-18 10:51:42 +0200

    ver.2 для работы с датами используем Date

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,14 @@
 // если сегодня четное число - она должна отвечать "Привет, (имя)!"
 // если нечетное - "Здраствуй, (имя)!"
 // testing vcs
-// ver.2 using Time
+// ver.1 using Calendar and Date
 
 
-import java.time.LocalDate;
-import java.time.MonthDay;
-
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Scanner;
 
-public class Main {
 
-    public static void main(String[] args){
-// Ввод имени из косноли
-        Scanner in = new Scanner(System.in); //создаём объект класса Scanner
-        System.out.print("Введите имя: ");
-        String name = in.nextLine(); // считывает строку из потока ввода и сохраняет в переменную
-// Вывод даты
-        System.out.println("Сегодня: " + LocalDate.now());
-// объявление переменной dayOfMonth
-        MonthDay m = MonthDay.now();
-        int dayOfMonth = m.getDayOfMonth();
-// Проверка даты на четность
-// Вывод приветствия в косоли
-
-        if ((dayOfMonth%2) == 0)
-        {
-            System.out.println("Привет, " + name);// четное число
-        }
-        else
-        {
-            System.out.println("Здравствуй, " + name);//нечетное число
-        }
-    }
-}
-
-/*
 public class Main {
 
     public static void main(String[] args){
@@ -61,5 +34,5 @@ public class Main {
         }
     }
 }
-*/
+
 
