4f0b29532252ca94b78b2dd5bbc2e7a2ecb7d5d8
Emin Aliiev <emin.aliev@gmail.com>
2015-11-18 10:43:39 +0200

    пробую time

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,41 @@
 // если сегодня четное число - она должна отвечать "Привет, (имя)!"
 // если нечетное - "Здраствуй, (имя)!"
 // testing vcs
-// ver.1 using Calendar
+// ver.2 using Time
 
 
-import java.util.Calendar;
-import java.util.Date;
+import java.time.LocalDate;
+import java.time.MonthDay;
+
 import java.util.Scanner;
-// import java.time.*;
 
+public class Main {
+
+    public static void main(String[] args){
+// Ввод имени из косноли
+        Scanner in = new Scanner(System.in); //создаём объект класса Scanner
+        System.out.print("Введите имя: ");
+        String name = in.nextLine(); // считывает строку из потока ввода и сохраняет в переменную
+// Вывод даты
+        System.out.println("Сегодня: " + LocalDate.now());
+// объявление переменной dayOfMonth
+        MonthDay m = MonthDay.now();
+        int dayOfMonth = m.getDayOfMonth();
+// Проверка даты на четность
+// Вывод приветствия в косоли
+
+        if ((dayOfMonth%2) == 0)
+        {
+            System.out.println("Привет, " + name);// четное число
+        }
+        else
+        {
+            System.out.println("Здравствуй, " + name);//нечетное число
+        }
+    }
+}
+
+/*
 public class Main {
 
     public static void main(String[] args){
@@ -34,4 +61,5 @@ public class Main {
         }
     }
 }
+*/
 
