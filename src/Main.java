40f55d6f7b277d980323c6e86fcd7708577b1cf9
Emin Aliiev <emin.aliev@gmail.com>
2015-11-18 10:54:49 +0200

    последняя подчистка перед сдачей

--- src/Main.java
+++ src/Main.java
@@ -36,30 +36,5 @@ public class Main {
     }
 }
 
-/*
-public class Main {
 
-    public static void main(String[] args){
-// Ввод имени из косноли
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите имя: ");
-        String name = in.nextLine();
-// Вывод даты
-        System.out.println("Сегодня: " + new Date());
-// объявление переменной cal
-        Calendar cal = Calendar.getInstance();
-// Проверка даты на четность
-// Вывод приветствия в косоли
-
-        if ((cal.get(Calendar.DAY_OF_MONTH)%2) == 0)
-        {
-            System.out.println("Привет, " + name);// четное число
-        }
-        else
-        {
-            System.out.println("Здравствуй, " + name);//нечетное число
-        }
-    }
-}
-*/
 
